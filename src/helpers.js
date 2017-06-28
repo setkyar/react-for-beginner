2694fe1e1306822fc326abeec9c74a91677df7ea
Set Kyar Wa Lar <setkyar16@gmail.com>
2017-06-28 19:08:03 +0630

    helpers and utility

--- src/helpers.js
+++ src/helpers.js
@@ -9,7 +9,7 @@ export function rando(arr) {
 export function slugify(text) {
   return text.toString().toLowerCase()
     .replace(/\s+/g, '-')           // Replace spaces with -
-    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+    .replace(/[^\w\-]+/g, '')       // 
     .replace(/\-\-+/g, '-')         // Replace multiple - with single -
     .replace(/^-+/, '')             // Trim - from start of text
     .replace(/-+$/, '');            // Trim - from end of text
