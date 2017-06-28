612f782849aae726945c8b46ab83760addce95cf
Set Kyar Wa Lar <setkyar16@gmail.com>
2017-06-28 17:39:28 +0630

    Loading Css into react app

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,14 +3,14 @@ import React from 'react';
 class StorePicker extends React.Component {
 	render() {
 		return (
-			{ /* Hello */ }
 			<form className="store-selector">
+				{ /* Hello */ }
 				<h2>Please Enter A Store</h2>
 				<input type="text" required placeholder="Store Name" />
 				<button type="submit">Visist Store →</button>
 			</form>
 		)
-	}	
+	}
 }
 
 export default StorePicker;
