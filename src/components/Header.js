86e2a0d6dcfac601b8b95a27e21471838b6f0e1f
Set Kyar Wa Lar <setkyar16@gmail.com>
2017-06-28 18:08:13 +0630

    pass data with props

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 class Header extends React.Component {
 	render() {
+		console.log(this);
 		return (
-			<p>Header</p>
+			<header className="top">
+				<h1>Catch of the Day</h1>
+				<h3 className="tagLine">{this.props.tagline}</h3>
+			</header>
 		)
 	}
 }
