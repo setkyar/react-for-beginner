e7e8c5f6cd3f62b0a08418f75cf4b38485d36977
Set Kyar Wa Lar <setkyar16@gmail.com>
2017-06-29 12:45:36 +0630

    working with react event, all about react router and understanding state

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,16 +2,30 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+	// constructor() {
+	// 	super();
+	// 	this.goToStore = this.goToStore.bind(this);
+	// }
+	goToStore(event) {
+		event.preventDefault();
+		const storeId = this.storeInput.value;
+		this.context.router.transitionTo(`/store/${storeId}`);
+	}
 	render() {
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore.bind(this)}>
 				{ /* Hello */ }
 				<h2>Please Enter A Store</h2>
-				<input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+				<input type="text" required placeholder="Store Name" 
+					defaultValue={getFunName()} ref={(input) => {this.storeInput = input}}/>
 				<button type="submit">Visist Store →</button>
 			</form>
 		)
 	}
+}
+
+StorePicker.contextTypes = {
+	router: React.PropTypes.object
 }
 
 export default StorePicker;
