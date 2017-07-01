039ae55acc08ea5569016ea48d7cfadb88354497
Set Kyar Wa Lar <setkyar16@gmail.com>
2017-07-01 11:22:58 +0630

    updating order state

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
 
 		this.addFish = this.addFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
+		this.addToOrder = this.addToOrder.bind(this);
 
 		// get initial state
 		this.state = {
@@ -32,6 +33,13 @@ class App extends React.Component {
 		})
 	}
 
+	addToOrder(key) {
+		const order = {...this.state.order};
+		order[key] = order[key] + 1 || 1;
+
+		this.setState({ order })
+	}
+
 	render() {
 		return (
 			<div className="catch-of-the-day">
@@ -41,7 +49,9 @@ class App extends React.Component {
 						{
 							Object
 								.keys(this.state.fishes)
-								.map(key => <Fish key={key} details={this.state.fishes[key]}/>)
+								.map(key => <Fish key={key} index={key}
+									details={this.state.fishes[key]}
+									addToOrder={this.addToOrder}/>)
 						}
 					</ul>
 				</div>
